e538bf8fb72f8705036d184aeaa049ca193e4544
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2024-11-08 03:03:04 +0100

    update

---
+++ Project Python/fibonacci.py
@@ -0,0 +1,16 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print("Fibonacci sequence")
+n = int(input("Enter the numbers of terms: "))
+
+fib = []
+for i in range(n):
+    fib.append(fibonacci(i))
+print(fib)
