29f884c8131a02a21114a9b8ab5922e2e23a0b00
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2024-11-09 01:06:06 +0100

    Update radioactive_decay.py

--- Project Python/radioactive_decay.py
+++ Project Python/radioactive_decay.py
@@ -14,7 +14,7 @@ N_B = zeros(N)
 N_C = zeros(N)
 
 # Constants
-k1 = 0.2
+k1 = 0.3
 k2 = 0.2
 
 # Initial conditions
@@ -23,12 +23,11 @@ N_A[0] = N_A0
 N_B[0] = 0
 N_C[0] = 0
 
-
 # Derivative function
 for i in range(1, N):
     N_A[i] = N_A[i-1] - dt*k1*N_A[i-1]
     N_B[i] = N_B[i-1] + dt*(k1*N_A[i-1] - k2*N_B[i-1])
-    N_C[i] = N_C[i-1] + dt*k2*N_B[i-1]
+    N_C[i] = N_C[i-1] + dt*k2*N_B[i-1] 
     t[i] = t[i-1] + dt
 
 # Plot
@@ -38,7 +37,7 @@ plt.plot(t, N_C, label='C')
 plt.legend(['A', 'B', 'C']) 
 plt.xlabel('$x$')
 plt.ylabel('$y$')
-plt.title('Finite Difference')
+plt.title('Chain of Radioactive Decay')
 plt.show()
 
 ## Radiative balance: k1*N_A = k2*N_B
