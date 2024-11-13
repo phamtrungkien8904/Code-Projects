8e7a53e1d102c57d838d720cb4149c2e375ee121
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2024-11-13 01:23:10 +0100

    Update projectile_2.py

--- projectile_2.py
+++ projectile_2.py
@@ -1,45 +1,59 @@
+from math import sin, cos, radians
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 # Time step for the animation (s), max time to animate for (s).
 dt, tmax = 0.01, 5
-# Signal frequency (s-1), decay constant (s-1).
-f, alpha = 2.5, 1
 # These lists will hold the data to plot.
-t, M, E, NE = [], [], [], []
+t, x, y, vx, vy = [], [], [], [], []
+# Initial parameters.
+v0 = 10
+theta = radians(45)  # Convert angle to radians
+g = 9.81
+h0 = 5
+
+# Initial conditions.
+vx.append(v0 * cos(theta))
+vy.append(v0 * sin(theta))
+x.append(0)
+y.append(h0)
+t.append(0)
 
 # Draw an empty plot, but preset the plot x- and y-limits.
 fig, ax = plt.subplots()
-line1, = ax.plot([], [], linestyle='-')  # Set the line style to dashed
-line2, = ax.plot([], [], linestyle='--')   # Set the line style to solid
-line3, = ax.plot([], [], linestyle='--')  # Set the line style to dash-dot
-ax.set_xlim(0, tmax)
-ax.set_ylim(-1, 1)
-ax.set_xlabel('t /s')
-ax.set_ylabel('M (arb. units)')
+line, = ax.plot([], [])
+ax.set_xlim(0, 20)
+ax.set_ylim(0, 20)
+ax.set_xlabel('x /m')
+ax.set_ylabel('y /m')
+
+# Create a text object to display the time
+time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
 
 def init():
-    return line1, line2, line3
+    time_text.set_text('')
+    return line, time_text
 
-def animate(i, t, M, E, NE):
+def animate(i, x, y):
     """Draw the frame i of the animation."""
 
     # Append this time point and its data and set the plotted line data.
-    T = i*dt
-    t.append(T)
-    M.append(np.sin(2*np.pi*f*T) * np.exp(-alpha*T))
-    E.append(np.exp(-alpha*T))
-    NE.append(-np.exp(-alpha*T))
-    line1.set_data(t, M)
-    line2.set_data(t, E)
-    line3.set_data(t, NE)
-    return line1, line2, line3
+    _t = i * dt
+    t.append(_t)
+    x.append(vx[0] * _t)
+    y.append(y[0] + vy[0] * _t - 0.5 * g * _t**2)
+    line.set_data(x, y)
+    
+    # Update the time text
+    time_text.set_text(f'Time = {_t:.2f} s')
+    
+    return line, time_text
 
 # Interval between frames in ms, total number of frames to use.
 interval, nframes = 1000 * dt, int(tmax / dt)
 # Animate once (set repeat=False so the animation doesn't loop).
 ani = animation.FuncAnimation(fig, animate, frames=nframes, init_func=init,
-                  fargs=(t, M, E, NE), repeat=False, interval=interval, blit=True)
+                              fargs=(x, y), repeat=False, interval=interval, blit=True)
 plt.grid()
 plt.show()
