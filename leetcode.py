401af8f1186519e6127cd37fc3c96dda1bf5fbb7
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2024-11-24 02:09:19 +0100

    Update leetcode.py

--- leetcode.py
+++ leetcode.py
@@ -1,11 +1,17 @@
 class Solution(object):
-    def romanToInt(self, s):
-        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        result = 0
-        for i in range(len(s)):
-            if i > 0 and roman[s[i]] > roman[s[i - 1]]:
-                result += roman[s[i]] - 2 * roman[s[i - 1]]
-            else:
-                result += roman[s[i]]
-        return result
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+        for i in range(len(strs[0])):
+            for string in strs[1:]:
+                if i >= len(string) or string[i] != strs[0][i]:
+                    return strs[0][:i]
+        return strs[0]
 
+# Test cases
+s = Solution()
+print(s.longestCommonPrefix(["flower","flow","flight"])) # "fl"
