0f442796230f0f8f84b116e21398b516acb6e7d6
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2024-12-19 08:28:25 +0100

    update

--- test.py
+++ test.py
@@ -1,30 +1,11 @@
 from datetime import datetime
-from plyer import notification
-import schedule
-import time
 
-def show_notification():
-    # Today's date
-    today = datetime.now()
+# Today's date
+today = datetime.today()
 
-    # Target date
-    target_date = datetime(2025, 3, 2)
+# Target date
+target_date = datetime(2025, 3, 2)
 
-    # Days remaining
-    days_left = (target_date - today).days
-
-    # Display notification
-    notification.notify(
-        title="Loa Loa!!!",
-        message=f"Còn {days_left} ngày để hai em bé gặp nhau.",
-        app_name="Vietnam Coundown",
-        timeout=10
-    )
-
-# Schedule the notification at 6:00 PM every day
-schedule.every().day.at("18:09").do(show_notification)
-
-print("Notification scheduler is running...")
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+# Difference in days
+days_until = (target_date - today).days
+print(days_until)
