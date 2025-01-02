ade4ed1279c3c8db46bc0c22d8dff25429672df8
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2025-01-02 13:27:34 +0100

    up

--- days-count.py
+++ days-count.py
@@ -3,9 +3,11 @@ from datetime import datetime
 # Today's date
 today = datetime.today()
 
+start_date = datetime(2024, 6, 12)
+
 # Target date
 target_date = datetime(2025, 3, 2)
 
 # Difference in days
-days_until = (target_date - today).days
-print(days_until)
+days_duration = (target_date - today).days
+print(days_duration)
