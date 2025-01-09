3379385c34bb8783ed04b5c5a625c1a2253be9e4
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2025-01-09 05:52:33 +0100

    up

---
+++ Project C++/header.h
@@ -0,0 +1,13 @@
+#ifndef header_h
+#define header_h   
+
+#include <iostream>
+#include <string>
+
+namespace eP {
+    void print(std::string str) {
+        std::cout << str << std::endl;
+    }
+}
+
+#endif
--- Project C++/test.cpp
+++ Project C++/test.cpp
@@ -1,41 +1,6 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-
-using namespace std;
-
-vector<double> squares(int n) {
-    vector<double> result;
-    for (int i = 0; i < n; i++) {
-        result.push_back(i * i);
-    }
-    return result;
-}
-
-int factorial(int n){
-    if (n == 0) {
-        return 1;
-    }
-    return n * factorial(n - 1);
-    if (n<0){
-        cout << "Error: negative number" << endl;
-    }
-}
-
-
+#include "header.h"
 
 int main() {
-    for (double d : squares(15)) {
-        cout << d << " ";
-    }
-    cout << endl;
-    vector<int> v = {1,3,4,2,5,7,9};
-    sort(v.begin(), v.end());
-    for (int i : v) {
-        cout << i << " ";
-    }
-    cout << endl;
-    cout << factorial(5) << endl;
+    eP::print("Hello, World!");
     return 0;
 }
-
