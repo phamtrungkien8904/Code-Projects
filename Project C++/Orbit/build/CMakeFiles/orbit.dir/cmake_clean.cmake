8bcdbec09b1a52676ff7cb4a91be206faa20a2df
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2025-03-14 17:04:42 +0700

    update

---
+++ Project C++/Orbit/build/CMakeFiles/orbit.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orbit.dir/test.cpp.o"
+  "CMakeFiles/orbit.dir/test.cpp.o.d"
+  "orbit"
+  "orbit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/orbit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
