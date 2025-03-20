da5821f468366d7368e8558dade576a431b7c191
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2025-03-20 17:56:49 +0700

    up

--- Project C++/Magnetic-Bottle/build/CMakeFiles/magneticbottle.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for magneticbottle.
-# This may be replaced when dependencies are built.
--- Project C++/Test/math.cpp
+++ Project C++/Test/math.cpp
@@ -1,9 +1,6 @@
 #include "mymath.h"
 using namespace std;
 
-int main() {
-    MyMath math;
-    cout << math.Factorial(5) << endl;
-    cout << math.Factorial(10) << endl; 
-    return 0;
+int mySqrt(int x){
+    
 }
