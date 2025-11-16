0cfa456ae2f9f94c7f0e73b22470c599910ce544
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2025-11-16 18:59:41 +0100

    up

--- Project Python/Quantum/schroedinger.py
+++ Project Python/Quantum/schroedinger.py
@@ -1,2 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+m = 3
+n = 5
+A = np.zeros((m, n), dtype=complex)
+B = np.zeros((m, n), dtype=complex)
+C = np.zeros((m, n), dtype=complex)
+
+def func(x,t):
+    C = np.zeros((m, n), dtype=complex)
+    for i in range(m):
+        for j in range(n):
+            C[i,j] = i + j 
+    return C[x][t]
+C = func(1, 2)
+print(C)
--- Project Python/Quantum/test.py
+++ Project Python/Quantum/test.py
@@ -1,7 +1,16 @@
 import numpy as np
 
-n = 3
-A = np.zeros((n, n))
-A[1,2] = 5
-B = np.ones((n, n))
-print(A)
+m = 3
+n = 5
+A = np.zeros((m, n),dtype=complex)
+B = np.zeros((m, n),dtype=complex)
+
+def func(x,t):
+    C = np.zeros((m, n),dtype=complex)
+    for i in range(m):
+        for k in range(n):
+            C[i][k] = i + 1j*k 
+    return C[x][t]
+C = func(1, 2)
+
+print(C)
