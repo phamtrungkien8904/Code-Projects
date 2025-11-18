bb6a1bd33eb225ada0a881af44e4cdd7c331f095
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2025-11-18 02:19:10 +0100

    up

--- Project Python/Quantum/free-particle.py
+++ Project Python/Quantum/free-particle.py
@@ -2,25 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-# General wave function: psi(x,t) = A(x,t) exp[i(kx - wt)]
+# Free quantum wave packet
+
+# Constant
+hbar = 1.0  # Reduced Planck's constant
+m = 1.0    # Particle mass
+
 
 
 # Time steps
-dt = 0.01
-t_max = 10
-Nt = int(t_max / dt)
-dx = 0.01
-x_max = 10
-Nx = int(x_max / dx)
+dt = 0.1
+t_min = 0
+t_max = 50
+Nt = int((t_max - t_min) / dt)
+dx = 0.1
+x_min = 0
+x_max = 50
+Nx = int((x_max - x_min) / dx)
 
-c = 1  # wave speed (group velocity)
-w = 1 # angular frequency
-k = 10  # wave number
-#k = w/c # dispersion relation, group velocity = phase velocity = c
-A = 1
+# Parameters
 
-t = np.linspace(0, t_max, Nt)
-x = np.linspace(0, x_max, Nx)
+k = 5  # wave number
+w = hbar * k**2 / (2 * m)  # angular frequency
+vG = hbar * k / m  # group velocity
+alpha = 1  # packet width
+beta = hbar/(2*m) # dispersion coefficient
+
+
+t = np.linspace(t_min, t_max, Nt)
+x = np.linspace(x_min, x_max, Nx)
 psi = np.zeros((Nx, Nt), dtype=complex)
 
 def wave():
@@ -28,8 +38,9 @@ def wave():
     psi = np.zeros((Nx, Nt), dtype=complex)
     for i in range(0, Nx):
         for j in range(0, Nt):
-            psi[i][j] = A * np.exp(-(x[i] - c*t[j])**2 + 1j * (k * x[i] - w * t[j]))
-    return psi  # shape (Nx, Nt)
+            psi[i][j] = np.exp(1j*(k*x[i] - w*t[j]))/np.sqrt(alpha + 1j*beta*t[j]) * np.exp(-(x[i] - vG*t[j])**2/(4*(alpha + 1j*beta*t[j])))
+    C = np.sqrt(np.sum(np.abs(psi[:,0])**2)*dx)  # Normalization constant
+    return psi/C  # shape (Nx, Nt)
 
 psi = wave()  # shape (Nx, Nt)
 
@@ -38,7 +49,7 @@ fig, ax = plt.subplots()
 line1, = ax.plot([], [], lw=2, color='red')
 line2, = ax.plot([], [], lw=2, color='blue')
 line3, = ax.plot([], [], lw=2, color='green')
-ax.set_xlim(0, x_max)
+ax.set_xlim(x_min, x_max)
 ax.set_ylim(-1.5, 1.5)
 ax.set_xlabel('Position')
 ax.set_ylabel('Amplitude')
@@ -46,10 +57,10 @@ ax.set_ylabel('Amplitude')
 def animate(i):
     line1.set_data(x, np.real(psi[:, i]))
     line2.set_data(x, np.imag(psi[:, i]))
-    line3.set_data(x, np.abs(psi[:, i]))
+    line3.set_data(x, np.abs(psi[:, i])**2)
     return line1, line2, line3
 
-interval = 10* dt 
+interval =  1000*dt 
 nframes = int(Nt)
 ani = animation.FuncAnimation(fig, animate, frames=nframes, repeat=False, interval=interval, blit=True)
 # ani.save('wave.gif', writer='pillow', fps=30)
