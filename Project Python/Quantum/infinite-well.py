b3268c2a137d0754385a9ab68627d27f9c3f6d64
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2025-11-23 01:06:41 +0100

    Update infinite-well.py

--- Project Python/Quantum/infinite-well.py
+++ Project Python/Quantum/infinite-well.py
@@ -3,11 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-# Free quantum wave packet in infinite potential (Free space)
+# Free quantum wave packet in harmonic oscillator potential
 
 # Constant
 hbar = 1.0  # Reduced Planck's constant
-m = 10.0    # Particle mass
+m = 5.0    # Particle mass
 
 
 
@@ -16,19 +16,20 @@ dt = 0.1
 t_min = 0
 t_max = 50
 Nt = int((t_max - t_min) / dt) 
-dx = 0.1
+dx = 0.05
 x_min = -25
 x_max = 25
 Nx = int((x_max - x_min) / dx) 
 
 # Parameters
 
-k = 10  # wave number at center of packet
+k = 20  # wave number at center of packet
 # w = hbar * k**2 / (2 * m)  # angular frequency
 alpha = 0.5  # packet width
 p = hbar * k  # momentum
 V0 = p**2/(2*m)  # Kinetic energy
 x0 = -10  # Initial position
+K = 10  # Spring constant
 
 t = np.linspace(t_min, t_max, Nt + 1)
 x = np.linspace(x_min, x_max, Nx + 1)
@@ -36,6 +37,14 @@ x = np.linspace(x_min, x_max, Nx + 1)
 
 # Potential function
 V = np.zeros(Nx-1)
+
+
+
+
+# Configuration print 
+print(f'Wave number (at center): {k}')
+print(f'Packet width: {alpha}')
+
 
 # Solve engine
 def solve():
@@ -48,7 +57,7 @@ def solve():
     # Halmiltonian matrix
     lamb = hbar**2/(2*m*dx**2)
     H =lamb*(2*np.diag(np.ones(Nx-1),0) + (-1)*np.diag(np.ones(Nx-2),1) + (-1)*np.diag(np.ones(Nx-2),-1))
-    for i in range(Nx-2):
+    for i in range(Nx-1):
         H[i][i] += V[i]/lamb
     E,psi = np.linalg.eigh(H)  # Eigenvalue decomposition
     psi = psi.T  
@@ -69,7 +78,10 @@ def solve():
 Psi = solve() 
 
 
-# PLot test
+
+
+# Plot test
+
 plt.plot(x[1:-1], np.real(Psi[:,0]), lw=2, color='red')
 plt.plot(x[1:-1], np.imag(Psi[:,0]), lw=2, color='blue')
 plt.plot(x[1:-1], np.abs(Psi[:,0]), lw=2, color='green')
@@ -83,36 +95,55 @@ plt.show()
 
 
 
-fig, ax = plt.subplots()
-line1, = ax.plot([], [], lw=2, color='red')
-line2, = ax.plot([], [], lw=2, color='blue')
-line3, = ax.plot([], [], lw=2, color='green')
-line4, = ax.plot([], [], lw=1, color='black', linestyle='--')
-ax.legend(['Real', 'Imaginary', 'Magnitude'])
-ax.set_xlim(x_min, x_max)
-ax.set_ylim(-1.5, 1.5)
-ax.set_xlabel('Position')
-ax.set_ylabel('Amplitude')
-time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
+
+fig, (ax_wave, ax_heat) = plt.subplots(
+    2,
+    1,
+    figsize=(10, 8),
+    sharex=True,
+    gridspec_kw={"height_ratios": [1, 1]},
+)
+line1, = ax_wave.plot([], [], lw=2, color='red')
+line2, = ax_wave.plot([], [], lw=2, color='blue')
+line3, = ax_wave.plot([], [], lw=2, color='green')
+line4, = ax_wave.plot([], [], lw=1, color='black', linestyle='--')
+ax_wave.set_title('Infinite Well')
+
+
+
+
+ax_wave.legend(["Real", "Imaginary", "Magnitude"])
+ax_wave.set_xlim(x_min, x_max)
+ax_wave.set_ylim(-1.5, 1.5)
+ax_wave.set_ylabel("Amplitude")
+ax_heat.set_xlabel("Position")
+time_text = ax_wave.text(0.02, 0.95,  "", transform=ax_wave.transAxes)
+
+# Probability heat map
+Prob = ax_heat.imshow([np.abs(Psi[:,0])**2], extent=[x[1], x[-1], -1.5, 1.5], aspect='auto', cmap='hot', alpha=1, vmin=0)
+cbar = fig.colorbar(Prob, ax=[ax_wave, ax_heat], label='Probability Density', pad=0.02, fraction=0.05, shrink=0.45, anchor=(0.0, 0.0))
+ax_heat.fill_between(x[1:-1], 0, 1, where=V > 0, color='gray', alpha=0.5, transform=ax_heat.get_xaxis_transform(), label='Potential')
 
 def animate(i):
     line1.set_data(x[1:-1], np.real(Psi[:, i]))
     line2.set_data(x[1:-1], np.imag(Psi[:, i]))
     line3.set_data(x[1:-1], np.abs(Psi[:, i]))
-    
-    # Find peak magnitude
+    Prob.set_data([np.abs(Psi[:, i])**2])
+
+
     idx = np.argmax(np.abs(Psi[:, i]))
     peak_x = x[1:-1][idx]
     line4.set_data([peak_x, peak_x], [-1.5, 1.5])
-    
     time_text.set_text(f't={t[i]:.1f}s')
-    return line1, line2, line3, line4, time_text
+    return line1, line2, line3, line4, time_text, Prob
+
+###########
+###########
 
 nframes = int(Nt)
 interval =  100*dt
 ani = animation.FuncAnimation(fig, animate, frames=nframes, repeat=False, interval=interval, blit=True)
-plt.title('Quantum Wave Packet in Infinite Well')
 plt.show()
 
 
-# ani.save('tunnel.gif', writer='pillow', fps=30, dpi = 200) # Size  
+# ani.save('oscillator.gif', writer='pillow', fps=20, dpi = 200) # Size  
