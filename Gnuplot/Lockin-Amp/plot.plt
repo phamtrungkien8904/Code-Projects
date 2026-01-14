0daff46b1b1659c609197ceb1364e60a5f630600
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2026-01-14 23:20:53 +0100

    up

--- Gnuplot/Lockin-Amp/plot.plt
+++ Gnuplot/Lockin-Amp/plot.plt
@@ -9,18 +9,20 @@ set encoding utf8
 set title 'Signal Filter (RLC Bandpass)'
 set xlabel '$t$/ms'
 set ylabel '$U$/V'
-set xrange [2:6]
-set yrange [-2:2]
+set xrange [2:3]
+set yrange [-10:10]
 set sample 10000
 # set grid
 set datafile separator ','
 
 set style line 1 lt 1 lw 2 lc rgb 'red'
 set style line 2 lt 1 lw 2 lc rgb 'blue'
-set style line 3 lt 1 lw 2 lc rgb 'black' dt 2
+set style line 3 lt 1 lw 2 lc rgb 'green' 
+set style line 4 lt 1 lw 2 lc rgb 'black' dt 2
 
-
-plot 'data.csv' using ($1*1000):3 with lines linestyle 1 title 'Output Signal', \
-     'data.csv' using ($1*1000):2 with lines linestyle 2 title 'Input Signal'
+plot 'data.csv' using ($1*1000):3 with lines linestyle 1 title 'Reference Signal', \
+     'data.csv' using ($1*1000):2 with lines linestyle 2 title 'Input Signal',\
+    'data.csv' using ($1*1000):4 with lines linestyle 3 title 'Mix Signal',\
+    'data.csv' using ($1*1000):5 with lines linestyle 4 title 'Output Signal'
 
 # set out
--- Gnuplot/Lockin-Amp/plot.py
+++ Gnuplot/Lockin-Amp/plot.py
@@ -10,7 +10,7 @@ R = 220
 C = 2.2e-6
 tau = R*C  # Time constant
 dt = 0.001*tau   # Time step (dt << tau)
-t = np.arange(0, 0.01, dt)  # Time array
+t = np.arange(0, 0.003, dt)  # Time array
 f0 = 1/(2*np.pi*tau)  # Limit frequency
 
 # Generate the input signal (square wave)
@@ -36,7 +36,12 @@ f = f0 # Frequency of wave
 # u_in = 1*np.sin(2 * np.pi * f * t) + (1/5)*np.sin(2 * np.pi * 10 * f * t) + (1/5)*np.sin(2 * np.pi * 20 * f * t) + (1/5)*np.sin(2 * np.pi * 15 * f * t)
 
 # AM
-u_in = 1*np.sin(2 * np.pi * 5 * f * t) * (0.5*np.sin(2 * np.pi * 50*f * t) + 2*np.sin(2 * np.pi * 5*f * t))
+u_DC = 5
+u_sig = 2*np.sin(2 * np.pi * 50*f * t)
+u_noise = 0.5*np.sin(2 * np.pi * 100*f * t) + 0.2*np.sin(2 * np.pi * 150*f * t) + 0.1*np.sin(2 * np.pi * 200*f * t)
+u_ref = 1*np.sin(2 * np.pi * 50 * f * t)  # Reference signal with phase shift
+u_in = u_DC + u_sig + u_noise
+u_mix = u_in * u_ref
 
 # # AC sweep
 # f_start = 10
@@ -45,26 +50,18 @@ u_in = 1*np.sin(2 * np.pi * 5 * f * t) * (0.5*np.sin(2 * np.pi * 50*f * t) + 2*n
 # u_in = np.sin(2 * np.pi * (f_start + df*t*100000) * t) 
 
 # Apply the low-pass filter
-def low_pass_filter(u_in, tau, dt):
-    u_C = np.zeros_like(u_in)
-    for i in range(1, len(u_in)):
-        u_C[i] = (dt/tau)*u_in[i] + (1 - (dt/tau))*u_C[i - 1]
+def low_pass_filter(u):
+    u_C = np.zeros_like(u)
+    for i in range(1, len(u)):
+        u_C[i] = (dt/tau)*u[i] + (1 - (dt/tau))*u_C[i - 1]
     return u_C
 
-u_out = low_pass_filter(u_in, tau, dt)
-print(f0)
+u_out = low_pass_filter(u_mix)
 
-# # Transfer function (Amplitude)
-# def transfer_function(f, tau):
-#     s = 1j * 2 * np.pi * f
-#     H = 1 / (1 + s * tau)
-#     return abs(H) 
-
-# H_amp = transfer_function(f, tau)
 
 # Save the input and output signals to a CSV file
 with open("data.csv", "w", newline="") as csvfile:
     csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(["# Time", "Input", "Output"])
+    csvwriter.writerow(["# Time", "Input", "Reference", "Mix", "Output"])
     for i in range(len(t)):
-        csvwriter.writerow([t[i], u_in[i], u_out[i]])  
+        csvwriter.writerow([t[i], u_in[i], u_ref[i], u_mix[i], u_out[i]])  
