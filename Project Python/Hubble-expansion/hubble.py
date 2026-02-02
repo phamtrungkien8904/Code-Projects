6583906d07f00183a848cd634dbcb79f4ceedb1a
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2026-02-02 23:47:40 +0100

    Update hubble.py

--- Project Python/Hubble-expansion/hubble.py
+++ Project Python/Hubble-expansion/hubble.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 # Inputs
-ub = 10
+ub = 20
 lb = 0
 dt = 0.01
 N = int((ub - lb)/dt)
@@ -32,7 +32,11 @@ v[0] = 0
 # Derivative function
 for i in range(1, N):
     t[i] = t[i-1] + dt
-    v[i] = v[i-1] + r[i-1]*np.sqrt(8*pi*G*rho[i-1]/3 - k/r[i-1]**2)*dt
+    r_prev = max(r[i-1], 1e-12)
+    arg = 8*pi*G*rho[i-1]/3 - k/r_prev**2
+    if arg < 0:
+        arg = 0
+    v[i] = v[i-1] + r_prev*np.sqrt(arg)*dt
     r[i] = r[i-1] + dt*v[i-1]
     rho[i] = rho[0]*(r0/r[i])**3
 
