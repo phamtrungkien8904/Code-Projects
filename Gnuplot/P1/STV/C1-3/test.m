b35555aa72d7b88676cc7f3648f77e1398f6198a
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2026-03-07 17:25:53 +0100

    Update test.m

--- Gnuplot/P1/STV/C1-3/test.m
+++ Gnuplot/P1/STV/C1-3/test.m
@@ -1,8 +1,10 @@
 x=linspace(0,40,41);
-P=binopdf(x,40,0.5);
+P=100*binopdf(x,40,0.5);
 plot(x,P,'LineWidth',2,'Color','r')
 hold on
 stem(x,P,'LineWidth',2,'LineStyle','none','Color','r')
 xlabel('Erfolge')
 ylabel('Wahrscheinlichkeit')
+xlim([0 40])
+ylim([0 100])
 legend('Binomialverteilung')
