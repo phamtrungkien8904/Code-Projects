7890a47eba8368a276b8a2acf1b8c5eed41263cc
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2026-03-07 19:25:37 +0100

    up

--- Gnuplot/P1/STV/script.m
+++ Gnuplot/P1/STV/script.m
@@ -1,19 +1,25 @@
-T = readtable('data.csv','HeaderLines',0,'ReadVariableNames',true);
-m = T(:,2).';
-n = 12;
-p = 0.22;
+data = readtable('data.csv','ReadVariableNames',true);
 
-x=linspace(0,n,n+1);
-P=100*binopdf(x,n,p);
-plot(x,P,'LineWidth',2,'Color','r')
+% Metadata is stored in the first data row of columns n, N, p
+n = data{1,3};
+N = data{1,4};
+p = data{1,5};
+
+% Frequencies start from row 2 (coin count 0 to n)
+x = data{2:end,1}';
+m = data{2:end,2}' / N * 100;
+
+P = 100 * binopdf(x, n, p);
+plot(x, P, 'LineWidth', 2, 'Color', 'r')
 hold on
-stem(x,P,'LineWidth',2,'LineStyle','none','Color','r')
-hold on 
-plot(x,m,'LineWidth',2,'Color','b')
+stem(x, P, 'LineWidth', 2, 'LineStyle', 'none', 'Color', 'r')
 hold on
-stem(x,m,'LineWidth',2,'LineStyle','--','Color','b')
+plot(x, m, 'LineWidth', 2, 'Color', 'b')
+hold on
+stem(x, m, 'LineWidth', 2, 'LineStyle', '--', 'Color', 'b')
 xlabel('Erfolge')
 ylabel('Wahrscheinlichkeit(%)')
 xlim([0 n])
 ylim([0 100])
-legend('Binomialverteilung','','Messdaten','')
+title(sprintf('Data-generate vs. Theorie (N = %d)', N))
+legend('Binomialverteilung', '', 'Daten', '')
