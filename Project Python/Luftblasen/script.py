a378b027065e46d393bc69fb89cf5b0359e5191c
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2026-03-14 02:31:33 +0100

    up

--- Project Python/Luftblasen/script.py
+++ Project Python/Luftblasen/script.py
@@ -4,8 +4,8 @@ import csv
 
 ##### Air bubble in water #####
 # Parameters
-N = 1000  # Number of samples
-dt = 0.001  # Time step
+N = 100  # Number of samples
+dt = 0.0001  # Time step
 t = np.zeros(N)
 v = np.zeros(N)
 z = np.zeros(N)
@@ -14,11 +14,11 @@ rho0 = 1000  # Density of water (kg/m^3)
 rho = 1.2  # Density of air (kg/m^3)
 g = 9.81  # Gravitational acceleration (m/s^2)
 eta = 0.001  # Dynamic viscosity of water (Pa.s)
-R = 0.01  # Initial radius of the bubble (m)
+R = 0.002  # Initial radius of the bubble (m)
 
 for i in range(N):
     t[i] = i * dt
-    v[i] = v[i-1] + dt*((rho0/rho - 1)*g - 9*eta*v[i-1]/(2*rho*R**2))  # Update velocity using the equation of motion
+    v[i] = v[i-1] + dt*((rho0/rho - 1.0)*g - 9.0*eta*v[i-1]/(2.0*rho*R**2))  # Update velocity using the equation of motion
     z[i] = z[i-1] + dt*v[i]
 
 # Save the input and output signals to a CSV file
@@ -31,5 +31,11 @@ with open("data.csv", "w", newline="") as csvfile:
 plt.plot(t, v, label='Velocity')
 plt.xlabel('Time (s)')
 plt.ylabel('Velocity (m/s)')
+plt.legend()
+plt.show()
+
+plt.plot(t, z, label='Height')
+plt.xlabel('Time (s)')
+plt.ylabel('Height (m)')
 plt.legend()
 plt.show()
