e9540f9f2f9feba2f73b4b4f2864499ee7c26808
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2026-03-18 14:29:58 +0100

    Update eigenstate.py

--- Project Python/Quantum/eigenstate.py
+++ Project Python/Quantum/eigenstate.py
@@ -1,15 +1,10 @@
 # Solve Schrödinger equation for every 1D system using Finite Difference Method and Eigenvalue Decomposition
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-
-# Free quantum wave packet in harmonic oscillator potential
 
 # Constant
 hbar = 1.0  # Reduced Planck's constant
 m = 1.0    # Particle mass
-
-
 
 # Time steps
 dt = 0.1
@@ -17,19 +12,9 @@ t_min = 0
 t_max = 40
 Nt = int((t_max - t_min) / dt) 
 dx = 0.005
-x_min = -2
-x_max = 2
+x_min = -5
+x_max = 5
 Nx = int((x_max - x_min) / dx) 
-
-# Parameters
-
-k = 0  # wave number at center of packet
-# w = hbar * k**2 / (2 * m)  # angular frequency
-p = hbar * k  # momentum
-V0 = p**2/(2*m)  # Kinetic energy
-x0 = -10  # Initial position
-K = 1  # Spring constant
-
 t = np.linspace(t_min, t_max, Nt + 1)
 x = np.linspace(x_min, x_max, Nx + 1)
 
@@ -38,7 +23,7 @@ x = np.linspace(x_min, x_max, Nx + 1)
 V = np.zeros(Nx-1)
 for i in range(Nx-1):
     # V[i] = 0.5 * K * (x[i]**2)  # Harmonic oscillator potential
-    V[i] = 1000 if np.abs(x[i])>1 else 0  # infinite well potential
+    V[i] = 10 if np.abs(x[i])>1 else 0  # infinite well potential
     # V[i]=-0.5 if (np.abs(x[i])<5 and np.abs(x[i])>0.1) else 0 # Double-well potential
     # V[i] = -100 if np.abs(x[i])<0.1 else 0  # Delta-Function potential
 
@@ -49,17 +34,14 @@ lamb = hbar**2/(2*m*dx**2)
 H =lamb*(np.diag(2*np.ones(Nx-1) + V/lamb,0) + (-1)*np.diag(np.ones(Nx-2),1) + (-1)*np.diag(np.ones(Nx-2),-1))
 E,psi = np.linalg.eigh(H)  # Eigenvalue decomposition
 psi = psi.T  
+
 # Plot Eigenstates
-# plt.plot(x[1:-1], np.real(psi[3,:]), lw=2, label=f'n={3}, E={E[3]:.2f}')
-
-
 plt.plot(x[1:-1], V, lw=2, label='V(x)')
-for i in range(5):
-    plt.plot(x[1:-1], E[i] + 10*np.real(psi[i,:]), lw=2, label=f'n={i+1}, E={E[i+1]:.2f}')
+for i in range(10):
+    plt.plot(x[1:-1], E[i] + 50*np.real(psi[i,:]), lw=2, label=f'n={i+1}, E={E[i]:.2f}')
 plt.xlim(-2,2)
 plt.ylim(0,20)
 plt.legend()
-plt.show()
 plt.show()
 
 
