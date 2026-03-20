68c24a9c2e5e1f85b30470470cbce65621030974
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2026-03-20 11:34:47 +0100

    update

--- Project Python/Quantum/eigenstate.py
+++ Project Python/Quantum/eigenstate.py
@@ -23,7 +23,9 @@ x = np.linspace(x_min, x_max, Nx + 1)
 V = np.zeros(Nx-1)
 for i in range(Nx-1):
     # V[i] = 0.5 * K * (x[i]**2)  # Harmonic oscillator potential
-    V[i] = 20 if np.abs(x[i])>1 else 0  # infinite well potential
+    V[i] = 30 if x[i]<-1 else 0  # infinite well potential
+    V[i] = 0 if x[i]>-1 and x[i]<1 else V[i]
+    V[i] = 10*x[i]**2 if x[i]>0 else V[i]
     # V[i]=-0.5 if (np.abs(x[i])<5 and np.abs(x[i])>0.1) else 0 # Double-well potential
     # V[i] = -100 if np.abs(x[i])<0.1 else 0  # Delta-Function potential
 
@@ -38,11 +40,15 @@ psi = psi.T
 # Plot Eigenstates
 plt.plot(x[1:-1], V, lw=2, label='V(x)')
 for i in range(20):
-    plt.plot(x[1:-1], E[i] + 50*np.real(psi[i,:]), lw=2, label=f'n={i+1}, E={E[i]:.2f}')
+    plt.plot(x[1:-1], E[i] + 1000*np.abs(psi[i,:])**2, lw=2, label=f'n={i+1}, E={E[i]:.2f}')
+    plt.plot(x[1:-1], np.full_like(x[1:-1], E[i]), lw=1, ls='--', color='k')
 plt.xlim(-2,2)
-plt.ylim(0,30)
+plt.ylim(-10,50)
 # plt.legend()
 plt.show()
+
+
+plt.savefig('eigenstates.eps', format='eps')
 
 
 
