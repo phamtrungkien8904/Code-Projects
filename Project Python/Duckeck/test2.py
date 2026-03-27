bf2b891b21c1e703729198aadccde5a3a4ea2649
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2026-03-27 14:30:54 +0100

    update

--- Project Python/Duckeck/test2.py
+++ Project Python/Duckeck/test2.py
@@ -2,3 +2,11 @@ pattern = "CAGATT"
 with open("genom.txt", "r", encoding="utf-8") as f:
     count = sum(1 for line in f if pattern in line)
 print(count)
+
+pattern = "CAGATT"
+
+with open("genom.txt", "r", encoding="utf-8") as f:
+    genome = f.read().replace("\n", "").replace("\r", "")
+
+count = genome.count(pattern)  # non-overlapping matches
+print(count)
