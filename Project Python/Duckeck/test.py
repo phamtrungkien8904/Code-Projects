087d7de73e5a7adab444d8f4a73040603df6cd8d
Trung Kien Pham <Kien.Pham@campus.lmu.de>
2026-03-27 13:18:12 +0100

    update

--- Project Python/Duckeck/test.py
+++ Project Python/Duckeck/test.py
@@ -1,1 +1,3 @@
-print("Hello World!")
+with open('genom.txt', 'r') as f:
+    my_dict = dict(line.split() for line in f)  
+    # works only if file only contains lines that split into exactly 2 tokens
